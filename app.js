b0d07249f8fa6afeeaae750cfca4b52bbddec534
IvanHoliak <goliakivan@gmail.com>
2022-12-05 13:56:51 +0200

    Fix, updata

--- app.js
+++ app.js
@@ -2,14 +2,14 @@
 
 import { Command } from "commander";
 import Listr from "listr";
-import got from "got";
+import axios from 'axios';
 import * as cheerio from 'cheerio';
 
 const params = {
-    depth: 0,
     maxDepth: 3,
-    was: [],
-    hrefOrigin: null
+    hrefOrigin: null,
+    routes: [],
+    result: []
 };
 
 const commander = new Command();
@@ -36,13 +36,13 @@ commander
             {
                 title: "Process",
                 task: async(ctx) => {
-                    const result = await request(url);
+                    const result = await fetch(url);
                     if(!result) throw new Error;
 
                     const emails = result.reduce((acc, next) => {
                         if(next.emails.length){
                             for(let i = 0; i < next.emails.length; i++){
-                                if(!acc.includes(next.emails[i])) acc = [...acc, next.emails[i]];
+                                if(!acc.includes(next.emails[i]) && /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/.test(next.emails[i])) acc = [...acc, next.emails[i]];
                             };
                         };
                         return acc;
@@ -65,44 +65,86 @@ commander
 
 commander.parse();
 
-function url_is_valid(string){
-    const pattern = new RegExp('^(https?:\\/\\/)?'+
-    '((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|'+
-    '((\\d{1,3}\\.){3}\\d{1,3}))'+
-    '(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*'+
-    '(\\?[;&a-z\\d%_.~+=-]*)?'+
-    '(\\#[-a-z\\d_]*)?$','i');
-
-    return pattern.test(string);
+function url_is_valid(urlString){
+    try { 
+        return Boolean(new URL(urlString)); 
+    }catch(e){ 
+        return false; 
+    };
 };
 
-async function request(url){
-    try {
-        if(params.was.includes(url)) return null;
-        params.was.push(url);
+axios.interceptors.response.use(undefined, (err) => {
+    const { config, message } = err;
 
-        const result = [];
-        
-        const { body, statusCode } = await got.get(url);
-
-        const parseResult = await parser(body);
-        
-        result.push(parseResult);
-
-        if(params.depth < params.maxDepth && parseResult.routes.length) {
-            params.depth += 1;
-            for(let i = 0; i < parseResult.routes.length; i++){
-                const depth = url_is_valid(parseResult.routes[i]) 
-                    ? await request(parseResult.routes[i]) 
-                    : await request(`${params.hrefOrigin}${parseResult.routes[i]}`);
-                if(depth) result.push(...depth);
-            };
-        };
-        return result;
-    } catch(err) {
-        // console.log("Request error!");
-        // return new Error("Request error!");
+    if (!config || !config.retry) {
+        return Promise.reject(err);
     };
+
+    if (!(message.includes("timeout") || message.includes("Network Error"))) {
+        return Promise.reject(err);
+    };
+
+    config.retry -= 1;
+    const delayRetryRequest = new Promise((resolve) => {
+        setTimeout(() => {
+            // console.log("retry the request", config.url);
+            resolve();
+        }, config.retryDelay || 1000);
+    });
+    return delayRetryRequest.then(() => axios(config));
+});
+
+async function fetch(url, depth = 0){
+    try {
+
+        if(depth > params.maxDepth) return params.result;
+        
+        const {data, status} = await axios.get(url, {
+            retry: 3, 
+            retryDelay: 1000,
+            timeout: 3000
+        });
+
+        const parseResult = await parser(data);
+        
+        params.result.push(parseResult);
+
+        const originHref = new URL(url).origin;
+
+        /*-------------------*/
+        /*----Promise all----*/ 
+        /*-------------------*/
+        // Быстро
+
+        if(parseResult.routes.length){
+            const recResult = await recursive(parseResult.routes, depth, originHref);
+            if(recResult) params.result.push(...recResult);
+        };
+
+        /*-------------------*/
+        /*-------LOOP--------*/ 
+        /*-------------------*/
+        //Очень долго
+
+        // if(parseResult.routes.length){
+        //     for(let i = 0; i < parseResult.routes.length; i++){
+        //         const recResult = url_is_valid(parseResult.routes[i]) 
+        //         ? await fetch(parseResult.routes[i], depth + 1) 
+        //         : await fetch(`${params.hrefOrigin}${parseResult.routes[i]}`, depth + 1);
+        //         if(recResult) result.push(...recResult);
+        //     };
+        // };
+
+        return params.result;
+    } catch(err) {
+        return params.result;
+        // console.log("Request error!", url, depth);
+    };
+};
+
+async function recursive(routes, depth, origin) {
+    return Promise.allSettled(routes.map(route => url_is_valid(route) ? fetch(route, depth + 1) : fetch(`${origin}${route}`, depth + 1)))
+        .then(res => {});
 };
 
 function parser(html){
@@ -117,17 +159,21 @@ function parser(html){
 };
 
 function filterAnchors(anch){
+    const filenameRegExp = /\.(jpe?g|png|gif|bmp|webp|pdf|mp[3-4])$/i;
+
     const cont = anch.reduce((acc, next) => {
         if(next.includes("mailto:")) acc.emails.push(next.substring(7));
         if(!next.includes("mailto:") 
             && next !== "/" 
+            && !filenameRegExp.test(next)
             && !next.includes("/#") 
             && !next.includes("#") 
             && !next.includes("tel")){
             if(acc.routes.length) {
                 const isExist = acc.routes.some(route => route.replaceAll("/", "") === next.replaceAll("/", ""));
-                if(!isExist) acc.routes.push(next)
-            } else acc.routes.push(next);
+                next = /^\.?\.\//.test(next) ? next.replace(/^\.?\.\//, "/") : next;
+                if(!isExist) acc.routes.push(next.trim());
+            } else acc.routes.push(next.trim());
         };
 
         return acc;
@@ -135,6 +181,12 @@ function filterAnchors(anch){
         routes: [],
         emails: []
     });
+    
+    if(cont.routes.length){
+        cont.routes = cont.routes.filter(route => !params.routes.includes(route));
+    };
+
+    params.routes = [...new Set(...[params.routes.concat(cont.routes)])];
 
     return cont;
 };  
